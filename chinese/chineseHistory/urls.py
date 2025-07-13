a0ab5368cb301168ce782ad0b9806d4273d76f24
Raf-kun <14katosik03@gmail.com>
2025-07-13 20:13:33 +0300

    ve[t[t[

--- chinese/chineseHistory/urls.py
+++ chinese/chineseHistory/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 import views
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),  # ← маршрут главной страницы
-]
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('chineseHistory.urls')),
 ]
